97fd75f3f2de48784300dad58099b0d6cf19fe45
jeovazero <jeovapereira2000@live.com>
2020-09-28 02:45:00 -0300

    feat: add initial caani cli

--- src/Main.hs
+++ src/Main.hs
@@ -10,18 +10,57 @@ import Data.Maybe (fromMaybe)
 
 getResourceDir = fmap (fromMaybe "./resources") (lookupEnv "RESOURCE_DIR")
 
+data CLI
+  = CLIOutput String
+  | CLIError String
+  | CLIHelp
+
+help = unlines
+  [ "usage:"
+  , "      cat your-code.hs | caani -o image.png"
+  , ""
+  , "  -o OUTPUT_PATH        Specify the output path [Required]"
+  , "  -h                    Help"
+  , ""
+  , "Repository: https://github.com/jeovazero/caani"
+  , "XD"
+  ]
+
+parse :: [String] -> Maybe CLI -> CLI
+parse ("-h":_) _ = CLIHelp
+parse ("-o":x:xs) _ = parse xs (Just $ CLIOutput x)
+parse _ (Just cli) = cli
+parse _ _ = CLIError $ "Error: Missing or wrong arguments :(\n\n" ++ help
+
 main :: IO ()
 main = do
-  input <- TIO.getContents
   args <- getArgs
-  resourceDir <- getResourceDir
-  r <- caani (CaaniConfig {
-    boundary = (200, 1000),
-    fontPath = resourceDir ++ "/FiraCode-Medium.ttf",
-    tagPath = resourceDir ++ "/haskell-flag.png",
-    code = input,
-    outPath = "./caani-out.png"
-  })
-  case r of
-    Right () -> TIO.putStrLn "Success"  
-    Left err -> TIO.putStrLn . T.append "Error: " $ T.pack . show $ err
+  let cli = parse args Nothing
+  case cli of
+    CLIHelp -> TIO.putStrLn . T.pack $ help
+    CLIError err -> TIO.putStrLn . T.pack $ err
+    CLIOutput out -> do
+      input <- TIO.getContents
+      resourceDir <- getResourceDir
+      r <- caani (CaaniConfig {
+        boundary = (200, 1000),
+        fontPath = resourceDir ++ "/FiraCode-Medium.ttf",
+        tagPath = resourceDir ++ "/haskell-flag.png",
+        code = input,
+        outPath = out
+      })
+      case r of
+        Right () ->
+          TIO.putStrLn $ T.concat
+            [ "\n"
+            , "  Yay 🎉 !!! The image was saved in '"
+            , T.pack out
+            , "'\n"
+            ]
+        Left err ->
+          TIO.putStrLn $ T.concat
+            [ "\n"
+            , "Error 🙈: "
+            , T.pack . show $ err
+            , "\n"
+            ]
