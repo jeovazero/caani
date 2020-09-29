778cd2dcb1a1c2edef8d052cbbd16dcb705f7a7e
jeovazero <jeovapereira2000@live.com>
2020-09-29 02:43:26 -0300

    feat(artifact): add upx for compression

--- artifact.nix
+++ artifact.nix
@@ -3,15 +3,20 @@ let
   pkgs = import ./nix/source.nix { json = ./nix/source.json; };
   caani = import ./release.nix {};
   tar = pkgs.gnutar;
+  upx = pkgs.upx;
 in 
 pkgs.stdenv.mkDerivation {
     name = "artifact";
-    buildInputs = [ tar ];
+    buildInputs = [ tar upx ];
     src = ./resources;
     buildPhase = "mkdir -p $out/caani/resources";
     installPhase = ''
       cp . $out/caani/resources -rv
       cp ${caani}/bin/caani $out/caani
+      cd $out/caani
+      chmod 755 caani
+      upx caani
+      chmod 555 caani
       cd $out
       tar -czf caani.tar.gz caani/
     '';
